2d54d6da4157095ebe6bfb04906331296c61f37f
actyp <actypaldos@gmail.com>
2023-07-26 13:13:12 +0300

    Add exportToPDF(LearnerResult) in DotProcessor

--- src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/utils/DotProcessor.java
+++ src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/utils/DotProcessor.java
@@ -1,5 +1,6 @@
 package com.github.protocolfuzzing.protocolstatefuzzer.utils;
 
+import com.github.protocolfuzzing.protocolstatefuzzer.components.learner.LearnerResult;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -31,5 +32,26 @@ public class DotProcessor {
         } catch (IOException e) {
             LOGGER.warn("Could not export {} to {}: {}", dotFilename, pdfFilename, e.getMessage());
         }
+    }
+
+    /**
+     * Exports the learned file contained in the provided non-empty LearnerResult to PDF
+     * using {@link DotProcessor#exportToPDF(File)}.
+     * <p>
+     * If the contained DOT file is null, then a warning is logged.
+     *
+     * @param learnerResult  the LearnerResult to be used
+     */
+    public static void exportToPDF(LearnerResult learnerResult) {
+        if (learnerResult.isEmpty()) {
+            return;
+        }
+
+        if (learnerResult.getLearnedModelFile() == null) {
+            LOGGER.warn("The provided LearnerResult contains null DOT file");
+            return;
+        }
+
+        exportToPDF(learnerResult.getLearnedModelFile());
     }
 }
