63acb8ad62c736ada7a6541db15adeea116c6569
00oskpet <42684085+00oskpet@users.noreply.github.com>
2024-01-29 15:30:32 +0100

    add new class RAStateMachine for register automata
    Co-authored-by: Linus Olofsson <Kax-y@users.noreply.github.com>

---
+++ src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/components/learner/RAStateMachine.java
@@ -0,0 +1,50 @@
+package com.github.protocolfuzzing.protocolstatefuzzer.components.learner;
+
+import net.automatalib.alphabet.Alphabet;
+import de.learnlib.ralib.automata.RegisterAutomaton;
+import de.learnlib.ralib.words.ParameterizedSymbol;
+import de.learnlib.ralib.automata.util.RAToDot;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+
+public class RAStateMachine extends AbstractStateMachine {
+
+    protected RegisterAutomaton automata;
+    protected Alphabet<ParameterizedSymbol> alphabet;
+
+    public RAStateMachine(RegisterAutomaton automata, Alphabet<ParameterizedSymbol> alphabet) {
+        this.automata = automata;
+        this.alphabet = alphabet;
+    }
+
+    public Alphabet<ParameterizedSymbol> getAlphabet() {
+        return this.alphabet;
+    }
+
+    public void export(File graphFile) {
+        Boolean acceptingOnly = false; // TODO: should we just set this to false?
+        String dotString = (new RAToDot(this.automata, acceptingOnly)).toString();
+        
+        try (FileWriter fWriter = new FileWriter(graphFile, StandardCharsets.UTF_8)) {
+            fWriter.write(dotString);
+        } catch (IOException e) {
+            // TODO: Why not log exception?
+            LOGGER.warn("Could not export model to file: {}", graphFile.getAbsolutePath());
+        }
+    }
+
+    public RAStateMachine Copy() {
+        // FIXME: Figure out a way to copy a register automaton
+        RegisterAutomaton newAutomaton;
+
+        return new RAStateMachine(automata, alphabet);
+    }
+
+    @Override
+    public String toString() {
+        return this.automata.toString();
+    }
+}
