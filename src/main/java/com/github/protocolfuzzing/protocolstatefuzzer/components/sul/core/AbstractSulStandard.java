2086663c62626abd310c7ba3054ed507490b93b1
Kax-y <56028244+Kax-y@users.noreply.github.com>
2024-02-09 13:31:17 +0100

    Convert AbstractSul into interface and add implementations of it
    Co-authored-by: 00oskpet <42684085+00oskpet@users.noreply.github.com>

--- src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/components/sul/core/AbstractSulStandard.java
+++ src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/components/sul/core/AbstractSulStandard.java
@@ -3,20 +3,18 @@ package com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.config.SulConfig;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.core.sulwrappers.DynamicPortProvider;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.mapper.Mapper;
+import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.mapper.abstractsymbols.AbstractInput;
+import com.github.protocolfuzzing.protocolstatefuzzer.components.sul.mapper.abstractsymbols.AbstractOutput;
 import com.github.protocolfuzzing.protocolstatefuzzer.utils.CleanupTasks;
-import de.learnlib.sul.SUL;
 
 /**
- * Abstract class used as the SUL Oracle using the {@link Mapper} and the {@link SulAdapter}.
+ * Abstract class used as the SUL Oracle from {@link AbstractInput} to {@link AbstractOutput}
+ * using the {@link Mapper} and the {@link SulAdapter}.
  * <p>
  * Subclasses should initialize {@link #mapper} and {@link #sulAdapter} to
  * their own implementations.
- *
- * @param <I>  the type of inputs
- * @param <O>  the type of outputs
- * @param <E>  the type of execution context
  */
-public abstract class AbstractSul<I, O, E> implements SUL<I, O> {
+public abstract class AbstractSulStandard implements AbstractSul<AbstractInput, AbstractOutput> {
 
     /** Stores the constructor parameter. */
     protected SulConfig sulConfig;
@@ -28,7 +26,7 @@ public abstract class AbstractSul<I, O, E> implements SUL<I, O> {
     protected DynamicPortProvider dynamicPortProvider;
 
     /** Stores the Mapper instance. */
-    protected Mapper<I, O, E> mapper;
+    protected Mapper mapper;
 
     /** Stores the SulAdapter instance. */
     protected SulAdapter sulAdapter;
@@ -41,7 +39,7 @@ public abstract class AbstractSul<I, O, E> implements SUL<I, O> {
      * @param cleanupTasks  the cleanup tasks to run in the end
      *
      */
-    public AbstractSul(SulConfig sulConfig, CleanupTasks cleanupTasks) {
+    public AbstractSulStandard(SulConfig sulConfig, CleanupTasks cleanupTasks) {
         this.sulConfig = sulConfig;
         this.cleanupTasks = cleanupTasks;
         // mapper and sulAdapter will be provided in subclasses
@@ -54,35 +52,43 @@ public abstract class AbstractSul<I, O, E> implements SUL<I, O> {
      *
      * @return  the stored value of {@link #sulConfig}
      */
-    public abstract SulConfig getSulConfig();
+    public SulConfig getSulConfig() {
+        return sulConfig;
+    }
 
     /**
      * Returns the stored value of {@link #cleanupTasks}.
      *
      * @return  the stored value of {@link #cleanupTasks}
      */
-    public abstract CleanupTasks getCleanupTasks();
+    public CleanupTasks getCleanupTasks() {
+        return cleanupTasks;
+    }
 
     /**
      * Sets the value of {@link #dynamicPortProvider}.
      *
      * @param dynamicPortProvider  the dynamic port provider to be set
      */
-    public abstract void setDynamicPortProvider(DynamicPortProvider dynamicPortProvider);
+    public void setDynamicPortProvider(DynamicPortProvider dynamicPortProvider) {
+        this.dynamicPortProvider = dynamicPortProvider;
+    }
 
     /**
      * Returns the stored value of {@link #dynamicPortProvider}.
      *
      * @return  the stored value of {@link #dynamicPortProvider}
      */
-    public abstract DynamicPortProvider getDynamicPortProvider();
+    public DynamicPortProvider getDynamicPortProvider() {
+        return dynamicPortProvider;
+    }
 
     /**
      * Returns the stored value of {@link #mapper}.
      *
      * @return  the stored value of {@link #mapper}
      */
-    public Mapper<I, O, E> getMapper() {
+    public Mapper getMapper() {
         return mapper;
     }
 
@@ -91,5 +97,7 @@ public abstract class AbstractSul<I, O, E> implements SUL<I, O> {
      *
      * @return  the stored value of {@link #sulAdapter}
      */
-    public abstract SulAdapter getSulAdapter();
+    public SulAdapter getSulAdapter() {
+        return sulAdapter;
+    }
 }
