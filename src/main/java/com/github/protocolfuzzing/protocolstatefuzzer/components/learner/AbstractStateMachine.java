75da2ae9ee9d32861f10ad058e645c62c99c18a4
00oskpet <42684085+00oskpet@users.noreply.github.com>
2024-02-14 16:55:40 +0100

    Extend AbstractStateMachine with isNull and size

--- src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/components/learner/AbstractStateMachine.java
+++ src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/components/learner/AbstractStateMachine.java
@@ -4,13 +4,17 @@ import net.automatalib.alphabet.Alphabet;
 
 import java.io.File;
 
-public abstract class AbstractStateMachine<I, O> {
+public interface AbstractStateMachine<I, O> {
 
     abstract public void export(File graphFile);
 
     abstract public Alphabet<I> getAlphabet();
 
     abstract public AbstractStateMachine<I, O> copy();
+
+    abstract public boolean isNull();
+
+    abstract public int size();
 
     abstract public String toString();
 }
