42d602fd0beedab3d21f777916272e36ec64ae54
00oskpet <42684085+00oskpet@users.noreply.github.com>
2024-03-11 12:30:51 +0100

    Remove TODO:s

--- src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/components/sul/mapper/abstractsymbols/AbstractInputXml.java
+++ src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/components/sul/mapper/abstractsymbols/AbstractInputXml.java
@@ -54,7 +54,6 @@ public abstract class AbstractInputXml<O, P, E> extends AbstractSymbol implement
      * @param name  the symbol name to be set
      */
     @Override
-    // TODO: Pojoxml might need this, might not.
     protected void setName(String name) {
         this.xmlName = name;
     }
@@ -75,7 +74,6 @@ public abstract class AbstractInputXml<O, P, E> extends AbstractSymbol implement
      * @param extendedWait  the additional waiting time to be set
      */
     @Override
-    // TODO: Same as for setName
     public void setExtendedWait(Long extendedWait) {
         this.xmlExtendedWait = extendedWait;
     }
