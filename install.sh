2ec09644043e66b9f5139b18d489763c4fc0e8ec
Kostis Sagonas <kostis@it.uu.se>
2025-04-09 09:46:15 +0200

    Use a subshell in the install script

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@
 
 # This can be removed once RALib is available from a maven repository.
 git clone "https://github.com/LearnLib/ralib"
-cd ralib
-git checkout 6de3c3883fffdc0b0a1aba36365daddcc7ccaca5
-mvn install -DskipTests
-cd ..
+( cd ralib && git checkout 60d3a478e3d2289fcefde233ba5cdcf730812779 && mvn install -DskipTests )
 
 mvn install
