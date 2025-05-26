b975d4fab4eb0638b81b3cb4e864fbd54fb218f8
Oskar Pettersson Löfstedt <00oskpet@gmail.com>
2025-05-26 16:41:38 +0200

    Javadoc comments for MembershipOracleWrapper

--- src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/components/learner/oracles/MembershipOracleWrapper.java
+++ src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/components/learner/oracles/MembershipOracleWrapper.java
@@ -8,10 +8,21 @@ import de.learnlib.ralib.sul.SULOracle;
 import de.learnlib.ralib.words.PSymbolInstance;
 import net.automatalib.word.Word;
 
+
+/**
+ * A wrapper that allows the RALib SULOracle to be used where
+ * a MembershipOracle is required, since SULOracle does not
+ * implement this interface.
+ */
 public class MembershipOracleWrapper implements MembershipOracle<PSymbolInstance, Word<PSymbolInstance>> {
 
+    /** The wrapped oracle */
     private SULOracle wrappedOracle;
 
+    /**
+     * Constructs a wrapper around the provided instance
+     * @param wrappedOracle    The SULOracle to wrap
+     */
     public MembershipOracleWrapper(SULOracle wrappedOracle) {
         this.wrappedOracle = wrappedOracle;
     }
