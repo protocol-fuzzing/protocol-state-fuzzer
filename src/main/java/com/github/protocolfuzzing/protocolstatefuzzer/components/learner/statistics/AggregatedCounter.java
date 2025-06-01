cb0197c94e465c416a27b5b72c09e28538c716ab
Nanxi Chen <nanxichen1998@gmail.com>
2025-06-01 14:52:38 +0200

    fix javadoc

--- src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/components/learner/statistics/AggregatedCounter.java
+++ src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/components/learner/statistics/AggregatedCounter.java
@@ -4,10 +4,28 @@ import de.learnlib.filter.statistic.Counter;
 
 import java.util.List;
 
+/**
+ * Aggregates multiple counters for parallel execution scenarios.
+ * <p>
+ * The aggregated counter only supports read operations ({@link #getCount()}).
+ * Write operations ({@link #increment()}) should be performed on the individual
+ * counters that make up this aggregation.
+ * </p>
+ */
 public class AggregatedCounter extends Counter {
 
+    /** The list of individual counters to aggregate. */
     private final List<Counter> counters;
 
+    /**
+     * Constructs a new AggregatedCounter with the specified list of counters.
+     * <p>
+     * The aggregated counter will sum the counts from all provided counters
+     * when {@link #getCount()} is called.
+     * </p>
+     *
+     * @param counters a list of {@link Counter} instances to aggregate.
+     */
     public AggregatedCounter(List<Counter> counters) {
         super("AggregatedCounter", "#");
         this.counters = counters;
