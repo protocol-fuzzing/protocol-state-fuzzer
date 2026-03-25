bffcdf99a2abf8ad95c281dd3e58b679a928a7e4
actyp <actypaldos@gmail.com>
2026-03-25 18:17:39 -0400

    Reformat LearnerConfigRA and remove redundant method

--- src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/components/learner/config/LearnerConfigRA.java
+++ src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/components/learner/config/LearnerConfigRA.java
@@ -4,7 +4,6 @@ import com.beust.jcommander.Parameter;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.learner.factory.EquivalenceAlgorithmName;
 import com.github.protocolfuzzing.protocolstatefuzzer.components.learner.factory.LearningAlgorithmName;
 
-import java.io.PrintWriter;
 import java.util.Arrays;
 
 /**
@@ -34,16 +33,6 @@ public class LearnerConfigRA extends LearnerConfigStandard {
     protected Boolean disableIOMode = false;
 
     /**
-     * Returns the stored value of {@link #disableIOMode}.
-     *
-     * @return the stored value of {@link #disableIOMode}
-     */
-    @Override
-    public Boolean getDisableIOMode() {
-        return disableIOMode;
-    }
-
-    /**
      * Stores the JCommander Parameter -probNewDataValue
      * <p>
      * The probability of the Equivalence Oracle choosing a new data value
@@ -52,16 +41,6 @@ public class LearnerConfigRA extends LearnerConfigStandard {
      */
     @Parameter(names = "-probNewDataValue", description = "The probability of some RA equivalence algorithms to choose a new data value")
     protected Double probNewDataValue = 0.8;
-
-    /**
-     * Returns the stored value of {@link #disableIOMode}.
-     *
-     * @return the stored value of {@link #disableIOMode}
-     */
-    @Override
-    public Double getProbNewDataValue() {
-        return probNewDataValue;
-    }
 
     /**
      * Stores the JCommander Parameter -maxRuns
@@ -74,16 +53,6 @@ public class LearnerConfigRA extends LearnerConfigStandard {
     protected Integer maxRuns = 10000;
 
     /**
-     * Returns the stored value of {@link #maxRuns}.
-     *
-     * @return the stored value of {@link #maxRuns}
-     */
-    @Override
-    public Integer getMaxRuns() {
-        return maxRuns;
-    }
-
-    /**
      * Stores the JCommander Parameter -maxDepthRA
      * <p>
      * The maximum depth for some RA equivalence algorithms
@@ -94,16 +63,6 @@ public class LearnerConfigRA extends LearnerConfigStandard {
     protected Integer maxDepthRA = 10;
 
     /**
-     * Returns the stored value of {@link #maxDepthRA}.
-     *
-     * @return the stored value of {@link #maxDepthRA}
-     */
-    @Override
-    public Integer getMaxDepthRA() {
-        return maxDepthRA;
-    }
-
-    /**
      * Stores the JCommander Parameter -resetRuns
      * <p>
      * Whether or not to reset runs.
@@ -112,16 +71,6 @@ public class LearnerConfigRA extends LearnerConfigStandard {
      */
     @Parameter(names = "-resetRuns", description = "Whether or not to reset runs.")
     protected Boolean resetRuns = false;
-
-    /**
-     * Returns the stored value of {@link #resetRuns}.
-     *
-     * @return the stored value of {@link #resetRuns}
-     */
-    @Override
-    public Boolean getResetRuns() {
-        return resetRuns;
-    }
 
     /**
      * Stores the JCommander Parameter -SeedTransitions
@@ -138,16 +87,6 @@ public class LearnerConfigRA extends LearnerConfigStandard {
     protected Boolean seedTransitions = false;
 
     /**
-     * Returns the stored value of {@link #seedTransitions}.
-     *
-     * @return the stored value of {@link #seedTransitions}
-     */
-    @Override
-    public Boolean getSeedTransitions() {
-        return seedTransitions;
-    }
-
-    /**
      * Stores the JCommander Parameter -drawSymbolsUniformly
      * <p>
      * Whether or not symbols should be drawn uniformly. This affects how the
@@ -161,6 +100,66 @@ public class LearnerConfigRA extends LearnerConfigStandard {
     protected Boolean drawSymbolsUniformly = false;
 
     /**
+     * Returns the stored value of {@link #disableIOMode}.
+     *
+     * @return the stored value of {@link #disableIOMode}
+     */
+    @Override
+    public Boolean getDisableIOMode() {
+        return disableIOMode;
+    }
+
+    /**
+     * Returns the stored value of {@link #disableIOMode}.
+     *
+     * @return the stored value of {@link #disableIOMode}
+     */
+    @Override
+    public Double getProbNewDataValue() {
+        return probNewDataValue;
+    }
+
+    /**
+     * Returns the stored value of {@link #maxRuns}.
+     *
+     * @return the stored value of {@link #maxRuns}
+     */
+    @Override
+    public Integer getMaxRuns() {
+        return maxRuns;
+    }
+
+    /**
+     * Returns the stored value of {@link #maxDepthRA}.
+     *
+     * @return the stored value of {@link #maxDepthRA}
+     */
+    @Override
+    public Integer getMaxDepthRA() {
+        return maxDepthRA;
+    }
+
+    /**
+     * Returns the stored value of {@link #resetRuns}.
+     *
+     * @return the stored value of {@link #resetRuns}
+     */
+    @Override
+    public Boolean getResetRuns() {
+        return resetRuns;
+    }
+
+    /**
+     * Returns the stored value of {@link #seedTransitions}.
+     *
+     * @return the stored value of {@link #seedTransitions}
+     */
+    @Override
+    public Boolean getSeedTransitions() {
+        return seedTransitions;
+    }
+
+    /**
      * Returns the stored value of {@link #drawSymbolsUniformly}.
      *
      * @return the stored value of {@link #drawSymbolsUniformly}
@@ -168,39 +167,5 @@ public class LearnerConfigRA extends LearnerConfigStandard {
     @Override
     public Boolean getDrawSymbolsUniformly() {
         return drawSymbolsUniformly;
-    }
-
-    @Override
-    public void printRunDescriptionSelf(PrintWriter printWriter) {
-        printWriter.println("LearnerConfigStandard Parameters");
-        printWriter.println("Alphabet: " + getAlphabetFilename());
-        printWriter.println("Learning Algorithm: " + getLearningAlgorithm());
-        printWriter.println("Equivalence Algorithms: " + getEquivalenceAlgorithms());
-        printWriter.println("Max Depth: " + getMaxDepth());
-        printWriter.println("Min Length: " + getMinLength());
-        printWriter.println("Max Length: " + getMaxLength());
-        printWriter.println("Max Equivalence Queries: " + getEquivQueryBound());
-        printWriter.println("Runs Per Membership Query: " + getRunsPerMembershipQuery());
-        printWriter.println("Random Length: " + getRandLength());
-        printWriter.println("Membership Query Retries: " + getMembershipQueryRetries());
-        printWriter.println("Log Queries: " + isLogQueries());
-        printWriter.println("Prob Reset: " + getProbReset());
-        printWriter.println("Test File: " + getTestFile());
-        printWriter.println("Seed: " + getSeed());
-        printWriter.println("Cache Tests: " + isCacheTests());
-        printWriter.println("Ce Sanitization: " + isCeSanitization());
-        printWriter.println("Skip Non Det Tests: " + isSkipNonDetTests());
-        printWriter.println("Ce Reruns: " + getCeReruns());
-        printWriter.println("Probabilistic Sanitization: " + isProbabilisticSanitization());
-        printWriter.println("Time Limit: " + getTimeLimit());
-        printWriter.println("Test Limit: " + getTestLimit());
-        printWriter.println("Round Limit: " + getRoundLimit());
-        printWriter.println("IOMode: " + !getDisableIOMode());
-        printWriter.println("Probability of Choosing a New DataValue: " + getProbNewDataValue());
-        printWriter.println("Max Runs: " + getMaxRuns());
-        printWriter.println("Max Depth for Register Automata: " + getMaxDepthRA());
-        printWriter.println("Reset Runs: " + getResetRuns());
-        printWriter.println("Seed transitions: " + getSeedTransitions());
-        printWriter.println("Draw symbols uniformly: " + getDrawSymbolsUniformly());
     }
 }
