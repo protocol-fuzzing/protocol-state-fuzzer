5cf5de914116989b6941b3f42389d018cf460051
mave9159 <martin.skjerve@icloud.com>
2026-05-08 15:08:53 +0200

    feat: add interface DiffTesterConfigBuilder

--- src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/statefuzzer/difftester/config/DiffTesterConfigBuilder.java
+++ src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/statefuzzer/difftester/config/DiffTesterConfigBuilder.java
@@ -1,5 +1,14 @@
 package com.github.protocolfuzzing.protocolstatefuzzer.statefuzzer.difftester.config;
 
+/**
+ * Builder Interface for the DiffTesterConfig.
+ */
 public interface DiffTesterConfigBuilder {
 
+    /**
+     * Builds tha DiffTesterConfig.
+     *
+     * @return the DiffTesterConfig
+     */
+    DiffTesterConfig buildConfig();
 }
