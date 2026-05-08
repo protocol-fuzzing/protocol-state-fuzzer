885eb019a42e337ae5bce4cd8c70842f15d658c6
mave9159 <martin.skjerve@icloud.com>
2026-05-08 15:09:26 +0200

    feat: add interface DiffTesterConfig

--- src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/statefuzzer/difftester/config/DiffTesterConfig.java
+++ src/main/java/com/github/protocolfuzzing/protocolstatefuzzer/statefuzzer/difftester/config/DiffTesterConfig.java
@@ -1,5 +1,41 @@
 package com.github.protocolfuzzing.protocolstatefuzzer.statefuzzer.difftester.config;
 
+import com.github.protocolfuzzing.protocolstatefuzzer.statefuzzer.core.config.PropertyResolver;
+
+/**
+ * Interface for the configuration for differential testing.
+ */
 public interface DiffTesterConfig {
 
+    /**
+     * Returns the path to the first model file (modelA).
+     *
+     * @return the path to modelA
+     */
+    String getModelA();
+
+    /**
+     * Returns the path to the first model file (modelB).
+     *
+     * @return the path to modelB
+     */
+    String getModelB();
+
+    /**
+     * Returns the path to the alphabet, shared by modelA and modelB.
+     *
+     * @return the path to the alphabet
+     */
+    String getAlphabetFile();
+
+    /**
+     * Returns the singleton PropertyResolver instance.
+     * <p>
+     * Default: the singleton instance.
+     *
+     * @return the singleton PropertyResolver instance
+     */
+    default PropertyResolver getPropertyResolver() {
+        return PropertyResolver.getInstance();
+    }
 }
