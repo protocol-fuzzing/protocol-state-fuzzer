69bbba61f732bb2a7b4493ac0e26aa653094a312
Kostis Sagonas <kostis@it.uu.se>
2026-06-12 12:42:25 +0200

    Update LearnerConfigTest.java

--- src/test/java/com/github/protocolfuzzing/protocolstatefuzzer/components/learner/config/LearnerConfigTest.java
+++ src/test/java/com/github/protocolfuzzing/protocolstatefuzzer/components/learner/config/LearnerConfigTest.java
@@ -111,8 +111,7 @@ public class LearnerConfigTest<M> {
 
     private LearnerConfig[] parseWithStandard(StateFuzzerConfigBuilder stateFuzzerConfigBuilder, String[] partialArgs) {
         CommandLineParser<M> commandLineParser = new CommandLineParser<>(stateFuzzerConfigBuilder,
-            new DiffTesterConfigBuilderSimple(), null, null, null,
-            null);
+            new DiffTesterConfigBuilderSimple(), null, null, null, null);
 
         LearnerConfig[] learnerConfigs = new LearnerConfig[2];
 
@@ -193,8 +192,7 @@ public class LearnerConfigTest<M> {
 
     private void invalidParseWithEmpty(StateFuzzerConfigBuilder stateFuzzerConfigBuilder) {
         CommandLineParser<M> commandLineParser = new CommandLineParser<>(stateFuzzerConfigBuilder,
-            new DiffTesterConfigBuilderSimple(), null, null, null,
-            null);
+            new DiffTesterConfigBuilderSimple(), null, null, null, null);
 
         String[] partialArgs = new String[] {"-alphabet", "alphabetPath"};
 
